db339663c32ccc3e52ae539a76b89fc381cfc50a
Smooth4life <129492868+Smooth4life@users.noreply.github.com>
2025-02-06 13:52:10 +0900

    2025_02_06_Complete

--- Source/ShootingCPP/Private/ShootingGameModeBase.cpp
+++ Source/ShootingCPP/Private/ShootingGameModeBase.cpp
@@ -13,6 +13,12 @@ void AShootingGameModeBase::AddScore(int32 Point)
 {
 	CurrentScore += Point;
 
+	if (CurrentScore > BestScore)
+	{
+		BestScore = CurrentScore;
+		SaveBestScore();
+	}
+
 	PrintScore();
 }
 
@@ -37,6 +43,8 @@ void AShootingGameModeBase::BeginPlay()
 {
 	Super::BeginPlay();
 
+	LoadBestScore();
+
 	if (MainWidget != nullptr)
 	{
 		MainUI = CreateWidget<UMainWidget>(GetWorld(), MainWidget);
@@ -46,6 +54,8 @@ void AShootingGameModeBase::BeginPlay()
 			MainUI->AddToViewport();
 		}
 	}
+
+	PrintScore();
 }
 
 void AShootingGameModeBase::PrintScore()
@@ -59,10 +69,34 @@ void AShootingGameModeBase::PrintScore()
 
 void AShootingGameModeBase::LoadBestScore()
 {
-
+	bool bSuccess = false;
+	UShootingGameInstance* ShootingGameInstance = Cast<UShootingGameInstance>(GetGameInstance());
+	if (ShootingGameInstance)
+	{
+		UGameSaveManeger* SaveManager = ShootingGameInstance->GetSaveGameManager();
+		if (SaveManager)
+		{
+			UShootingGameSaveData* SaveData = SaveManager->LoadGameInstance(0, bSuccess);
+			if (bSuccess && SaveData)
+			{
+				BestScore = SaveData->BestScore;
+				UE_LOG(LogTemp, Warning, TEXT("Loaded Best Scor : %d"), BestScore);
+			}
+		}
+	}
 }
 
 void AShootingGameModeBase::SaveBestScore()
 {
+	UShootingGameInstance* GameInstance = Cast<UShootingGameInstance>(GetGameInstance());
 
+	if (GameInstance) 
+	{
+		UGameSaveManeger* SaveManager = GameInstance->GetSaveGameManager();
+		if (SaveManager)
+		{
+			SaveManager->SaveGameData(0, BestScore);
+			UE_LOG(LogTemp, Warning, TEXT("New Best Score Saved : %d"), BestScore);
+		}
+	}
 }
